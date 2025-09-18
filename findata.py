8c99a0c05cfb08137b2794337c099c936f4946fd
stebas101 <sbasurto@gmx.co.uk>
2025-09-18 14:12:30 +0100

    yfinance update

--- findata.py
+++ findata.py
@@ -2,16 +2,17 @@ import yfinance as yf
 import requests_cache
 
 
-def get_session() -> object:
-    session = requests_cache.CachedSession('yfinance.cache')
-    session.headers['User-agent'] = 'xoxstocks/0.1'
+# def get_session() -> object:
+#     session = requests_cache.CachedSession('yfinance.cache')
+#     session.headers['User-agent'] = 'xoxstocks/0.1'
     
-    return session
+#     return session
 
 
 def get_stock_info(symbol: str) -> dict:
-    session = get_session()
-    ticker = yf.Ticker(symbol, session=session)
+    # session = get_session()
+    # ticker = yf.Ticker(symbol, session=session)
+    ticker = yf.Ticker(symbol)
     info: dict = ticker.get_info()
     
     return {'name'      : info.get('shortName'),
